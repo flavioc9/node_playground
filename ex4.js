c7e6b949a49ddff4d50dfaabd1e71786c7660edf
flavioc9 <flaviocosta999@gmail.com>
2021-06-21 19:14:36 +0100

    ex4

--- ex4.js
+++ ex4.js
@@ -1,6 +1,26 @@
-const math = require("./mathModule")
+const http = require("http");
+const url = require("url");
+const querystring = require("querystring");
+const math = require("./mathModule");
 
-console.log(math.sum(1, 2))
-console.log(math.sub(1, 2))
-console.log(math.mul(1, 2))
-console.log(math.div(1, 2))
+const port = 3000;
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  const parsed = url.parse(req.url, true).query;
+
+  const a = parseInt(parsed.a);
+  const b = parseInt(parsed.b);
+
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`
+        <ul>
+            <li>${a} + ${b} = ${math.sum(a, b)}</li>
+            <li>${a} - ${b} = ${math.sub(a, b)}</li>
+            <li>${a} x ${b} = ${math.mul(a, b)}</li>
+            <li>${a} / ${b} = ${math.div(a, b)}</li>
+        </ul>
+    `);
+});
+
+server.listen(port);
